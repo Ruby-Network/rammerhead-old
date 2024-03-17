ae4d25d14666764499127e41f940a9bf4059d3c2
MotorTruck1221 <motortruck1221@protonmail.com>
2024-03-17 04:30:04 -0600

    final touches

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const rammerheadScopes = [ "/rammerhead.js", "/hammerhead.js", "/transport-worker.js", "/task.js", "/iframe-task.js", "/worker-hammerhead.js", "/messaging", "/sessionexists", "/deletesession", "/newsession", "/editsession", "/needpassword", "/syncLocalStorage", "/api/shuffleDict", "/mainport" ];
 const rammerheadSession = /^\/[a-z0-9]{32}/;
+const { createServer } = require('http');
+const createRammerhead = require('./src/server/index.js');
+const rh = createRammerhead()
 function shouldRouteRh(req) {
   const url = new URL(req.url, "http://0.0.0.0");
+    console.log(url.pathname);
   return (rammerheadScopes.includes(url.pathname) || rammerheadSession.test(url.pathname));
 }
 function routeRhRequest(req, res) { rh.emit("request", req, res) }
@@ -25,7 +29,7 @@ const proxyHandler = (handler, opts) => {
 
 const fastify = require('fastify')({
   logger: false,
-  severFactory: proxyHandler
+  serverFactory: proxyHandler
 })
 
 fastify.get('/', (request, reply) => {
